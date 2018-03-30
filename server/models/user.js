624821070d4dfdf25339462400ab2a6fc9352f5d
SrAxi <riccardo_polacci@hotmail.com>
2018-03-30 18:17:05 +0200

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -81,6 +81,26 @@ UserSchema.pre('save', function (next) {
     }
 });
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    const User = this;
+
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    })
+};
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -116,9 +116,7 @@ app.post('/users', (req, res) => {
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
-        res
-            .header('x-auth', token)
-            .send(user);
+        res.header('x-auth', token).send(user);
     }).catch((e) => res.status(400).send(e))
 });
 
@@ -127,6 +125,18 @@ app.post('/users', (req, res) => {
  */
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
