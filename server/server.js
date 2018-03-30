d1d3dd7ad5dcab282c4ec54bf48a75e041a4bdef
SrAxi <riccardo_polacci@hotmail.com>
2018-03-30 14:00:22 +0200

    Added PATCH /todos:id and lodash to libraries.

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
@@ -77,8 +78,35 @@ app.delete('/todos/:id', (req, res) => {
     }).catch(() => {
         res.status(400).send();
     });
+});
 
+/**
+ * PATCH /todos/:id
+ */
+app.patch('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const body = _.pick(req.body, ['text', 'completed']);
 
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch(() => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
