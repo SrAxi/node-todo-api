1249507bebf6148ad0a55d942ef47ba2371a67f7
SrAxi <riccardo_polacci@hotmail.com>
2018-03-30 15:03:38 +0200

    Setup environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,6 @@ mongoose.Promise = global.Promise;
 
 // Connect to DB
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -7,7 +9,7 @@ const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { Users } = require('./models/users');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const app = express();
 
 app.use(bodyParser.json());
