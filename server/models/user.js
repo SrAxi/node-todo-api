37c7b07ce84a0159663d049c2d44be365ba565b5
SrAxi <riccardo_polacci@hotmail.com>
2018-03-30 16:57:26 +0200

    Added password hashing

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -64,6 +65,21 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
+    } else {
+        next();
+    }
+});
 
 const User = mongoose.model('User', UserSchema);
 
