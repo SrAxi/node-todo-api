05e4f4ee295bab471433b3643ce49ebf4a39dec5
SrAxi <riccardo_polacci@hotmail.com>
2018-04-22 13:06:13 +0200

    Open API to all (we still use JWT security)

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,16 +1,16 @@
 // Add headers
 const setHeaders = (req, res, next) => {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    // Website I wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // Request methods you wish to allow
+    // Request methods I wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
+    // Request headers I wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-  // Set to true if you need the website to include cookies in the requests sent
+    // Set to true if I need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
