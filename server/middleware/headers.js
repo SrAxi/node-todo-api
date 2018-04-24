ba50eedb0a51c23a5fecf40dec5e24d49c6bc155
SrAxi <riccardo_polacci@hotmail.com>
2018-04-24 23:01:54 +0200

    Allowing header for external Apps

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -5,17 +5,20 @@ const setHeaders = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods I wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
 
-    // Request headers I wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Response headers I wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, x-auth');
 
     // Set to true if I need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
+    // Request headers I wish to expose
+    res.setHeader('Access-Control-Expose-Headers', 'x-auth');
+
+    // Pass to next layer of middleware
+    next();
 };
 
 module.exports = { setHeaders };
